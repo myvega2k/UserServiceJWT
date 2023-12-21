a2015b16a01fa9a9c49503b9e0e5a59da333d66d
myvega2k <vega2k@kakao.com>
2023-12-21 09:18:21 +0900

    USERDEdeatilsservice add

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,42 +21,52 @@ import com.example.userservice.vo.ResponseOrder;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService{
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	BCryptPasswordEncoder passwordEncoder;
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	ModelMapper mapper;
-	
-	@Override
-	public UserDto createUser(UserDto userDto) {
-		userDto.setUserId(UUID.randomUUID().toString());
-		
-		UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-		//userEntity.setEncryptedPwd("encrypted_password");
-		userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
-		userRepository.save(userEntity);
-		
-		return mapper.map(userEntity, UserDto.class);
-	}
-	
-	@Override
-	public UserDto getUserByUserId(String userId) {
-		UserEntity userEntity = userRepository.findByUserId(userId);
-		if (userEntity == null)
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username);
+        if (userEntity == null)
+            throw new UsernameNotFoundException(username + ": not found");
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                new ArrayList<>());
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        //userEntity.setEncryptedPwd("encrypted_password");
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
+        userRepository.save(userEntity);
+
+        return mapper.map(userEntity, UserDto.class);
+    }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if (userEntity == null)
             throw new UsernameNotFoundException("User not found");
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
         List<ResponseOrder> orderList = new ArrayList<>();
         userDto.setOrders(orderList);
-		return userDto;
-	}
-	
-	@Override
-	public Iterable<UserEntity> getUserByAll() {		
-		return userRepository.findAll();
-	}
+        return userDto;
+    }
+
+    @Override
+    public Iterable<UserEntity> getUserByAll() {
+        return userRepository.findAll();
+    }
 }
