c8298a695169d24956d13cfd39d513024ce63c8d
myvega2k <vega2k@kakao.com>
2023-12-21 10:43:43 +0900

    jwt token create

--- src/main/java/com/example/userservice/filter/MyAuthenticationFilter.java
+++ src/main/java/com/example/userservice/filter/MyAuthenticationFilter.java
@@ -1,8 +1,10 @@
 package com.example.userservice.filter;
 
+import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Slf4j
 public class MyAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -62,6 +65,17 @@ public class MyAuthenticationFilter extends UsernamePasswordAuthenticationFilter
         log.info(">>> 인증 성공");
         String userName
                 = ((User)authResult.getPrincipal()).getUsername();
+
+        UserDto userDetails = userService.getUserDetailsByEmail(userName);
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() +
+                        Long.parseLong(env.getProperty("token.expiration_time"))))
+                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512,
+                        env.getProperty("token.secret"))
+                .compact();
+        response.addHeader("token", token);
+        response.addHeader("userId", userDetails.getUserId());
 
     }
 }
